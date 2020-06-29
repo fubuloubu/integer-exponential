b6d4694e5f98065ed92f9997c0955ef2d7b259bb
Bryant Eisenbach <fubuloubu@gmail.com>
2020-06-29 19:51:29 -0400

    refactor: Discovered precision loss bug on decimal, increased to 42

--- int_exp.py
+++ int_exp.py
@@ -1,45 +1,28 @@
-from decimal import Decimal
+from decimal import Decimal, getcontext
 import math
 
+# Necessary to ensure we have enough precision to do the log/exp calcs
+getcontext().prec = 42
 
-PRECOMPUTED = {
-    "uint256": {
-        14: 319557,
-        13: 847179,
-        12: 2642245,
-        11: 10134188,
-        10: 50859008,
-        9: 365284284,
-        8: 4294967295,
-        7: 102116749982,
-        6: 6981463658331,
-        5: 2586638741762874,
-        4: 18446744073709551615,
-        3: 48740834812604276470692694,
-        2: 340282366920938463463374607431768211455,
-    },
-}
-
-
-def max_base(b: int, typ: str = "uint256") -> int:
+def max_base(b: int, num_bits: int, signed: bool) -> int:
     """
     For a given base `b`, compute the maximum value `a` can be
     to not produce an overflow in the equation `a ** b`
     """
-    integer_traits = typ.split("int")
-    value_bits = int(integer_traits[-1]) - (1 if "u" in typ else 0)
-    if not (int(integer_traits[-1]) % 8 == 0):
+    if b < 0:
+        raise ValueError("Value is negative, which we cannot handle")
+    elif not (num_bits % 8 == 0):
         raise ValueError("Type is not a modulo of 8")
-    elif b > value_bits:
+
+    value_bits = num_bits - (1 if signed else 0)
+    if b > value_bits:
         raise ValueError("Value is too large and will always throw")
     elif b < 2:
         return 2 ** value_bits - 1  # Maximum value for type
-    # All precomputed values computing using the below loop
-    # NOTE: Precalulated these because they take so long
-    elif typ in PRECOMPUTED and b in PRECOMPUTED[typ]:
-        return PRECOMPUTED[typ][b]
     else:
-        a = 2 ** math.ceil(Decimal(value_bits) / Decimal(b))  # Estimate
+        # Estimate (up to ~39 digits precision required)
+        a = math.ceil(2 ** (Decimal(value_bits) / Decimal(b)))
+        # Do a bit of iteration to ensure we have the exact number
         while (a + 1) ** b < 2 ** value_bits:
             a += 1
         while a ** b >= 2 ** value_bits:
--- tests/test_max_base.py
+++ tests/test_max_base.py
@@ -6,17 +6,16 @@ from int_exp import max_base
 
 @pytest.mark.parametrize("b", range(-1, 257))
 @pytest.mark.parametrize("signed", [True, False])
-@pytest.mark.parametrize("bits", range(8, 257, 8))
+@pytest.mark.parametrize("num_bits", range(8, 257, 8))
 @pytest.mark.timeout(1)  # Ensure this computes fast in all cases
-def test_max_base(b, signed, bits):
-    typ = f"{'u' if signed else ''}int{bits}"
-    if b > bits - int(signed):
+def test_max_base(b, num_bits, signed):
+    if b < 0 or b > num_bits - int(signed):
         with pytest.raises(ValueError):
-            a = max_base(b, typ=typ)
+            max_base(b, num_bits, signed)
     elif b < 2:
-        a = max_base(b, typ=typ)
-        assert a == 2 ** (bits - int(signed)) - 1
+        a = max_base(b, num_bits, signed)
+        assert a == 2 ** (num_bits - int(signed)) - 1
     else:
-        a = max_base(b, typ=typ)
-        assert a ** b < 2 ** (bits - int(signed))
-        assert (a + 1) ** b >= 2 ** (bits - int(signed))
+        a = max_base(b, num_bits, signed)
+        assert a ** b < 2 ** (num_bits - int(signed))
+        assert (a + 1) ** b >= 2 ** (num_bits - int(signed))
