949773728e13af3d3b14be5e10c0012c62db67d4
Bryant Eisenbach <fubuloubu@gmail.com>
2020-06-29 19:55:43 -0400

    feat: Add function to detect maximum exponent too

--- int_exp.py
+++ int_exp.py
@@ -1,8 +1,33 @@
-from decimal import Decimal, getcontext
+from decimal import Decimal, DivisionByZero, InvalidOperation, getcontext
 import math
 
 # Necessary to ensure we have enough precision to do the log/exp calcs
 getcontext().prec = 42
+
+
+def max_exp(a: int, num_bits: int, signed: bool) -> int:
+    if not (num_bits % 8 == 0):
+        raise ValueError("Type is not a modulo of 8")
+
+    value_bits = num_bits - (1 if signed else 0)
+    if a >= 2 ** value_bits:
+        raise ValueError("Value is too large and will always throw")
+    elif a < -(2 ** value_bits):
+        raise ValueError("Value is too small and will always throw")
+
+    try:
+        b = int(Decimal(value_bits) / (Decimal(a).ln() / Decimal(2).ln()))
+    except (DivisionByZero, InvalidOperation):
+        return 1
+    if b <= 1:
+        return 1  # Value is assumed to be in range, therefore power of 1 is max
+    # Do a bit of iteration to ensure we have the exact number
+    while a ** (b + 1) < 2 ** value_bits:
+        b += 1
+    while a ** b >= 2 ** value_bits:
+        b -= 1
+    return b  # Exact
+
 
 def max_base(b: int, num_bits: int, signed: bool) -> int:
     """
--- tests/test_max_base.py
+++ tests/test_max_base.py
@@ -1,7 +1,9 @@
+from brownie.test import given
+from hypothesis import example, strategies as st, settings
 import pytest
 import itertools
 
-from int_exp import max_base
+from int_exp import max_base, max_exp
 
 
 @pytest.mark.parametrize("b", range(-1, 257))
@@ -19,3 +21,32 @@ def test_max_base(b, num_bits, signed):
         a = max_base(b, num_bits, signed)
         assert a ** b < 2 ** (num_bits - int(signed))
         assert (a + 1) ** b >= 2 ** (num_bits - int(signed))
+
+
+@given(a=st.integers(min_value=-(2 ** 127), max_value=2 ** 256 - 1))
+@example(a=0)
+@example(a=2 ** 127)
+@example(a=2 ** 127 - 1)
+@settings(max_examples=1000)  # per parametrized run
+@pytest.mark.parametrize("signed", [True, False])
+@pytest.mark.parametrize("num_bits", range(8, 257, 8))
+@pytest.mark.timeout(1)  # Ensure this computes fast in all cases
+def test_max_exp(a, num_bits, signed):
+    if a < -(2 ** (num_bits - int(signed))) or a >= 2 ** (num_bits - int(signed)):
+        with pytest.raises(ValueError):
+            max_exp(a, num_bits, signed)
+    else:
+        b = max_exp(a, num_bits, signed)
+        assert (
+            # No underflow for chosen value
+            a ** b >= -(2 ** (num_bits - int(signed)))
+            # No overflow for chosen value
+            and a ** b < 2 ** (num_bits - int(signed))
+        )
+        if b > 1:
+            assert (
+                # Either underflows
+                a ** (b + 1) < -(2 ** (num_bits - int(signed)))
+                # Or overflows for next value
+                or a ** (b + 1) >= 2 ** (num_bits - int(signed))
+            )
